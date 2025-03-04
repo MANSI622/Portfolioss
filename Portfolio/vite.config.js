8b2b489c54ac9bf488801aad939a236694218f13
Mansi_622 <137082748+MANSI622@users.noreply.github.com>
2025-03-04 19:39:50 +0530

    undo changes

--- Portfolio/vite.config.js
+++ Portfolio/vite.config.js
@@ -1,13 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Ensure correct path handling
-  build: {
-    outDir: 'dist',
-  },
-  server: {
-    historyApiFallback: true, // Ensure React Router works correctly
-  },
-});
+})
